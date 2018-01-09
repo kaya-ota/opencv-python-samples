5baf78a07d701ef02e5f78658971cb5f0394b254
28kayak <kaya.ota@gmail.com>
2018-01-09 22:04:04 +0900

    enabled to open video

--- video_capture/readme.md
+++ video_capture/readme.md
@@ -1,2 +1,8 @@
-Not Workning
-Problem 1: cannot open mp4 for some reasons. 
+##Install OpenCV to CentOS
+To install oencv into centos, read this article
+[Qiita](https://qiita.com/estaro/items/c2f8f790dc62a9f5965f)  
+  
+video_capture.py is a simple opencv program using web-camera.
+
+
+
--- video_capture/video_capture.py
+++ video_capture/video_capture.py
@@ -1,13 +1,23 @@
+
 import cv2
-import skvideo.io
+#import skvideo.io
 
+#to quit the program, push ESC key
+ESC = 27
 
+#video = "3D_B.JPG"
 
-video = "test1.mp4"
-cap = skvideo.io.VideoCapture(video)
-ret, frame = cap.read()
+#video = "SampleVideo_720x480_1mb.mp4"
+cap = cv2.VideoCapture(0)
+while(True):
+	ret, frame = cap.read()
+	print(ret)
+	if(cap.isOpened()== False):
+		print("Error openingvideo stream or file")
+	else:
+		print("opened successfully")
+	cv2.imshow("title", frame)
+	if cv2.waitKey(1)==ESC:
+		break
 
-if(cap.isOpened()== False):
-    print("Error openingvideo stream or file")
-else:
-    print("opened successfully")
+    
