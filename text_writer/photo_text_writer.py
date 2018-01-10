f06ee9c209a1bbd1bf5b1e5096ca86dea4801097
kaya-ota <32660238+kaya-ota@users.noreply.github.com>
2018-01-10 17:47:49 +0900

    simple change for better usage

--- text_writer/photo_text_writer.py
+++ text_writer/photo_text_writer.py
@@ -2,11 +2,15 @@
 import cv2 as cv
 
 
+#read a text file
+#get a number 
 
 print("image reading")
 img = cv.imread('sample.jpg')
 #cv.putText(img, 'HELLO', (0,50), cv.FONT_HERSHEY_PLAIN, 4, (0,0,0), 5, cv.LINE_AA)
 cv.putText(img, 'HELLO', (0,50),cv.FONT_HERSHEY_TRIPLEX, 2, (100,200,200), 5, cv.LINE_AA)
 cv.putText(img, 'WORLD!', (150,200),cv.FONT_HERSHEY_SCRIPT_COMPLEX, 2, (100,255,200), 5, cv.LINE_AA)
+#should automtically update n in sample-n.jpg 
 cv.imwrite('sample6.jpg', img)
+
 print("DONE !")
